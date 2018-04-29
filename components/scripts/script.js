b17f10d346a070300ba067bd47638bc66372ba9c
Andrei Gugeanu <andrei.gugy@gmail.com>
2018-04-29 15:31:46 +0300

    WebMagnat project commit

---
+++ components/scripts/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#vertical').lightSlider({
+        gallery: true,
+        vertical: true,
+        item: 1,
+        loop:true,
+        slideMargin: 0,
+        thumbItem: 8
+    });
+  });
